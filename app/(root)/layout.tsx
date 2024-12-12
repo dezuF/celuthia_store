8f6fe2e20d2d3cfe8de6cc525b9c7d390d5cab8b
dezuF <dezuF@users.noreply.github.com>
2024-12-12 09:09:07 -0400

    feat: add navbar component

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 
 import "../globals.css";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +20,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ClerkProvider>{children}</ClerkProvider>
+        <ClerkProvider>
+          <Navbar />
+          {children}
+        </ClerkProvider>
       </body>
     </html>
   );
