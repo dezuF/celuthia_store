122d6368e32089bae9170fdf3d590a2d7f8113ca
dezuF <dezuF@users.noreply.github.com>
2024-12-13 06:35:42 -0400

    feat: implement hook for managing the shopping cart.

--- components/ProductInfo.tsx
+++ components/ProductInfo.tsx
@@ -4,6 +4,8 @@ import { useState } from "react";
 import HeartFavorite from "./HeartFavorite";
 import { MinusCircle, PlusCircle } from "lucide-react";
 
+import useCart from "@/lib/hooks/useCart";
+
 const ProductInfo = ({ productInfo }: { productInfo: ProductType }) => {
   const [selectedColor, setSelectedColor] = useState<string>(
     productInfo.colors[0]
@@ -13,6 +15,8 @@ const ProductInfo = ({ productInfo }: { productInfo: ProductType }) => {
   );
 
   const [quantity, setQuantity] = useState<number>(1);
+
+  const cart = useCart();
 
   return (
     <div className="max-w-[400px] flex flex-col gap-4">
@@ -73,15 +77,33 @@ const ProductInfo = ({ productInfo }: { productInfo: ProductType }) => {
       <div className="flex flex-col gap-2">
         <p className="text-base-medium text-grey-2">Quantity:</p>
         <div className="flex gap-4 items-center">
-          <MinusCircle className="hover:text-red-1 cursor-pointer" onClick={() => quantity > 1 && setQuantity(quantity - 1)} />
+          <MinusCircle
+            className="hover:text-red-1 cursor-pointer"
+            onClick={() => quantity > 1 && setQuantity(quantity - 1)}
+          />
           <p className="text-body-bold">{quantity}</p>
-          <PlusCircle className="hover:text-red-1 cursor-pointer" onClick={() => setQuantity(quantity + 1)}/>
+          <PlusCircle
+            className="hover:text-red-1 cursor-pointer"
+            onClick={() => setQuantity(quantity + 1)}
+          />
         </div>
       </div>
 
-      <button className="outline text-base-bold py-3 rounded-lg hover:bg-black hover:text-white">Add To Cart</button>
+      <button
+        className="outline text-base-bold py-3 rounded-lg hover:bg-black hover:text-white"
+        onClick={() => {
+          cart.addItem({
+            item: productInfo,
+            quantity,
+            color: selectedColor,
+            size: selectedSize,
+          });
+        }}
+      >
+        Add To Cart
+      </button>
     </div>
-  )
+  );
 };
 
 export default ProductInfo;
