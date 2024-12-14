71a1c36104ecf677dfb395a5f59f6de8478bee9c
dezuF <dezuF@users.noreply.github.com>
2024-12-14 01:40:27 -0400

    feat: add Orders page with order list, details, and interactions

--- lib/actions/actions.ts
+++ lib/actions/actions.ts
@@ -6,9 +6,11 @@ export const getCollections = async () => {
 };
 
 export const getCollectionDetails = async (collectionId: string) => {
-  const collection = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/collections/${collectionId}`)
-  return await collection.json()
-}
+  const collection = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/collections/${collectionId}`
+  );
+  return await collection.json();
+};
 
 export const getProducts = async () => {
   const products = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products`);
@@ -16,11 +18,22 @@ export const getProducts = async () => {
 };
 
 export const getProductDetails = async (productId: string) => {
-    const product = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products/${productId}`)
-    return await product.json()
-  }
+  const product = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/products/${productId}`
+  );
+  return await product.json();
+};
 
-  export const getSearchedProducts = async (query: string) => {
-    const searchedProducts = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/search/${query}`)
-    return await searchedProducts.json()
-  }
+export const getSearchedProducts = async (query: string) => {
+  const searchedProducts = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/search/${query}`
+  );
+  return await searchedProducts.json();
+};
+
+export const getOrders = async (customerId: string) => {
+  const orders = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/orders/customers/${customerId}`
+  );
+  return await orders.json();
+};
